f70898db9827902fec84dc441eb8ff59a21fdfe7
LenoMac <hafizfullstack@gmail.com>
2024-04-12 17:23:33 +0600

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/header.css'
+
+export default function Header() {
+  return (
+    <div>
+        <div className="container">
+            <h3>Hello</h3>
+        </div>
+    </div>
+  )
+}
